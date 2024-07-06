d89e0e4886ee827622cdfa5811a694ff11bf8f8e
Priyanka Mahajan <pm01.03.14@gmail.com>
2024-07-07 00:43:20 +0530

    Added animation rotation for items

--- javascript.js
+++ javascript.js
@@ -1,9 +1,10 @@
 
-gsap.to("#String", {x: 20, duration: 1})
-gsap.to("#Disk", {x: 20, duration: 1})
+gsap.to("#String", {x: 20, duration: 1 , repeat: -1})
+gsap.to("#Disk", {scale:0.5, rotation: 360, transformOrigin: "center", ease: "none", duration: 3, repeat: -1})
 
-gsap.to("#RedBlink", {x: 20, duration: 1})
-gsap.to("#Round", {x: 20, duration: 1})
 
-gsap.to("#SideBook", {x: 20, duration: 1})
-gsap.to("#SlideDown", {y: 20, duration: 1})
+gsap.to("#RedBlink", {y: -20, duration: 1 , repeat: -1})
+gsap.to("#Round", {rotation: 360, transformOrigin: "center", ease: "none", duration: 3, repeat: -1})
+
+gsap.to("#SideBook", {x: 20, duration: 1 , repeat: -1})
+gsap.to("#SlideDown", {y: 20, duration: 1 , repeat: -1})
