37afccf4041917731fbd6697820dfdb5e6bf1286
Priyanka Mahajan <pm01.03.14@gmail.com>
2024-07-06 23:50:23 +0530

    first commit

---
+++ javascript.js
@@ -0,0 +1,9 @@
+
+gsap.to("#String", {x: 20, duration: 1})
+gsap.to("#Disk", {x: 20, duration: 1})
+
+gsap.to("#RedBlink", {x: 20, duration: 1})
+gsap.to("#Round", {x: 20, duration: 1})
+
+gsap.to("#SideBook", {x: 20, duration: 1})
+gsap.to("#SlideDown", {y: 20, duration: 1})
